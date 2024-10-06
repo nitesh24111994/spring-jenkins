2428c3eecd5d6c14503fdff701cd29a6d512b6be
nitesh24111994 <nitesh.andy45@gmail.com>
2024-10-06 13:36:41 +0530

    Revert "test code updated"
    This reverts commit 9960fd807b7b91638f52717b3efc9564068fc118.

--- src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
+++ src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
@@ -17,7 +17,6 @@ class JenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing second log statement...");
         Assertions.assertTrue(true);
 	}
 
