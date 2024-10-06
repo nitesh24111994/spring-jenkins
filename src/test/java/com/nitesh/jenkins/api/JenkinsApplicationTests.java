d7085c5190d60ba5e49d588a8d0549c7087f4a66
nitesh24111994 <nitesh.andy45@gmail.com>
2024-10-06 14:36:24 +0530

    Third commit

--- src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
+++ src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
@@ -17,6 +17,7 @@ class JenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing on second run...");
         Assertions.assertTrue(true);
 	}
 
