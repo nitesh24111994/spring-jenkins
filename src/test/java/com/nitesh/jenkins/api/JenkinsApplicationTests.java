a776f06599db9302e9bbe86f7c89c6f5de07e457
nitesh24111994 <nitesh.andy45@gmail.com>
2024-10-06 12:58:21 +0530

    second commit

--- src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
+++ src/test/java/com/nitesh/jenkins/api/JenkinsApplicationTests.java
@@ -1,13 +1,12 @@
 package com.nitesh.jenkins.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,7 +17,7 @@ class JenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
-        assertTrue(true);
+        Assertions.assertTrue(true);
 	}
 
 }
